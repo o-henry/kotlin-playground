cceaa1098ab6749454400fb3259db8c060f8a38d
henry <cksgod16@gmail.com>
2023-02-20 17:35:48 +0900

    add: 03-classes

---
+++ src/main/kotlin/javatokotlin/CH03/EmailAddress.kt
@@ -0,0 +1,35 @@
+package javatokotlin.CH03
+
+import java.util.*
+
+class EmailAddress(
+    val localPart: String, // primary constructor
+    val domain: String,
+) {
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (o == null || javaClass != o.javaClass) return false
+        val that = o as EmailAddress
+        return localPart == that.localPart && domain == that.domain
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(localPart, domain)
+    }
+
+    override fun toString(): String {
+        return "$localPart@$domain"
+    }
+
+    companion object {
+        fun parse(value: String): EmailAddress {
+            val atIndex = value.lastIndexOf('@')
+            require(!(atIndex < 1 || atIndex == value.length - 1)) { "EmailAddress must be two parts separated by @" }
+            return EmailAddress(
+                value.substring(0, atIndex),
+                value.substring(atIndex + 1)
+            )
+        }
+    }
+}
