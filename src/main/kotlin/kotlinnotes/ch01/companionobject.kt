0883b093d3bb9f0a3bc689443020b6c88c7b0d47
henry <cksgod16@gmail.com>
2023-03-30 22:36:24 +0900

    update

---
+++ src/main/kotlin/kotlinnotes/ch01/companionobject.kt
@@ -0,0 +1,9 @@
+package kotlinnotes.ch01
+
+class companionobject {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            println("Hello World")
+        }
+    }
+}
--- src/main/kotlin/reference/classes/01.kt
+++ src/main/kotlin/reference/classes/01.kt
@@ -1,6 +1,13 @@
 package reference.classes
 
 class Person constructor(name: String) {}
+class _Person(name: String) // primary constructor
+class __Person(val pets: MutableList<Pet> = mutableListOf())
+class Pet {
+    constructor(owner: __Person) {
+        owner.pets.add(this)
+    }
+}
 
 fun main() {
     val henry = Person(name = "Henry")
