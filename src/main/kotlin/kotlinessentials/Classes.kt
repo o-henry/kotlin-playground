f8e447e91f1784f6a79b3f07932dea1163898401
henry <cksgod16@gmail.com>
2023-04-18 00:00:30 +0900

    update

--- src/main/kotlin/kotlinessentials/Classes.kt
+++ src/main/kotlin/kotlinessentials/Classes.kt
@@ -6,8 +6,47 @@ class A {
     }
 }
 
+class User(val id: Int, val name: String)
+
+class Puppy(val name: String) {
+    inner class InnerPuppy {
+        fun think() {
+            println("Inner $name is thinking")
+        }
+    }
+}
+
+interface UserRepository {
+    fun findUser(id: Int): User?
+    fun addUser(user: User)
+}
+
+class FakeUserRepository : UserRepository {
+    private var users = mapOf<Int, User>()
+
+    override fun addUser(user: User) {
+        users = users.plus(user.id to user)
+    }
+
+    override fun findUser(id: Int): User? {
+        TODO("Not yet implemented")
+    }
+}
+
+
 fun main() {
     val a: A = A()
     println(a) // A@ADDRESS
     a.printMe() // A@ADDRESS (the same address)
+
+
+    /** user */
+    val user = User()
+    user.name = "norbert"
+    println(user.name) // NOBERT
+
+    /** puppyt */
+    val puppy = Puppy("Cookie")
+    val innerPuppy = puppy.InnerPuppy()
+    innerPuppy.think()
 }
