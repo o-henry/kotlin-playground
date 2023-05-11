573fb510cf737df0afe4cb8d44b82d45754ab218
henry <cksgod16@gmail.com>
2023-05-11 16:19:23 +0900

    coroutine.3 : Add comment to suspend function

--- src/main/kotlin/reference/coroutine/02.kt
+++ src/main/kotlin/reference/coroutine/02.kt
@@ -5,6 +5,18 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+/**
+ * main 의 출력은
+ * Hello
+ * World 1
+ * World 2
+ * Done
+ * 인데, Done은 왜 blocking 되었는지에 대해서 코멘트 해보자면,
+ * doWorld()는 coroutineScope로 정의되어 있고, coroutineScope는 모든 자식코루틴이 완료될때까지 일시 중단된다.
+ * 따라서 doWorld() 내부 launch 코루틴이 완료될 때까지 일시중단되어, println("Done")은 기다리게 된다.
+ *
+ * 즉 suspend와 같은 일시중단 함수를 사용하면 특정 지점에서 일시 중단되고, 다시 코루틴이 완료될때까지 기다리게 할 수 있다.
+ */
 fun main() = runBlocking {
     doWorld()
     println("Done")
